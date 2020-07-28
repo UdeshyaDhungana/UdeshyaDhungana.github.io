cf902b7a621812189bbc20d5f10b099966ae2255
Udeshya Dhungana <udeshyadhungana1@gmail.com>
2020-07-28 19:28:12 +0545

    changed jekyll theme

---
+++ _pages/about.md
@@ -0,0 +1,52 @@
+---
+permalink: /about/
+title: "About"
+---
+Hello there! I am a student of Computer Engineering at Pulchowk Campus, IOE.
+My interests are AI, Math, Linux and Full Stack Development.
+
+## Academic Background
+ - SLC: Pokhariya Higher Secondary School, Biratnagar
+ - +2: Arniko Awasiya Higher Secondary School, Biratnagar
+ - Ongoing Bachelor's in Computer Engineering: Pulchowk Campus    
+
+## Work Experiences
+
+- Home tuition in Biratnagar for 6 months: 2017-18
+- Competitive exams tutor for PEA institute: Summer, 2019    
+
+## Skills 
+
+- Teaching
+	- Maths
+	- Physics
+	- Computer Science
+ 
+- Web development
+	- HTML, CSS, JS
+	- Express
+	- React and its related libraries
+	- Jekyll
+	- Mongodb
+	- Front end design
+
+- Artificial Intelligence
+  - Machine Learning
+	- Neural Networks
+	- Unsupervised learning ( Beginner )
+- Linux ecosystem
+  - Using as main operating system for 3 years
+  - Shell scripting
+
+## Hobbies
+- Table tennis
+- PC gaming
+- Guitar  
+
+## Personal Life
+
+I was born in Jhorahat, a village near Biratnagar, Nepal.
+I have a younger brother. Both my parents are very supportive towards me. We moved to Biratnagar in 2013. Currently, I live in Kathmandu(2019-).  
+
+## Contact me
+You may reach out to me via the links in the footer.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "http://localhost:4000/blog/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "http://localhost:4000/blog/post-standard/",
+        "teaser": null
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "tags": ["chat","Post Formats"],
+        "url": "http://localhost:4000/blog/post-chat/",
+        "teaser": null
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "http://localhost:4000/blog/post-notice/",
+        "teaser": null
+      },{
+        "title": "Post: Quote",
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
+        "tags": ["Post Formats","quote"],
+        "url": "http://localhost:4000/blog/post-quote/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "The problem with tiktok",
+        "excerpt":"It was early 2018 when teens all around the world started using an app called Musically. It was a video sharing social media app founded by Alex Zhu, a Chinese entrepreneur . Mainly used for making lip sync videos, the app’s popularity skyrockted within months, if not, weeks. Suddenly, around...","categories": ["current-affairs"],
+        "tags": ["tiktok","data theft","technology"],
+        "url": "http://localhost:4000/current-affairs/tiktok-spyware/",
+        "teaser": "http://localhost:4000/assets/images/posts/tiktok.jpg"
+      }]
